982edecd3f2ef64266dd81a37738f7b8895f00eb
Thomas Neill <tommydneill@gmail.com>
2024-05-22 18:14:34 -0700

    fix: linePoly

--- content/geometry/LinePolyEdgeIntersection.h
+++ content/geometry/LinePolyEdgeIntersection.h
@@ -5,7 +5,7 @@
  * Source: thomas
  * Description: Calcs "delta points" for line s-e and edge of some polygon.
 *               To use, sort outputs by first key and maintain running sum of second key. 
-*               > 0 => in polygon.
+*               != 0 => in polygon.
  */
 vector<pair<P, ll>> linePoly(P s, P e, P p1, P p2) {
     ll s1 = sideOf(s,e,p1), s2 = sideOf(s,e,p2);

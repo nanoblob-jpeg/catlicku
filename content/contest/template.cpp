e0d3f0b0c3a4736e2ae8f6d08bffa09b840b3b46
codicon <elijahjhuang@gmail.com>
2024-07-03 11:48:16 -0700

    Current template

--- content/contest/template.cpp
+++ content/contest/template.cpp
@@ -1,14 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define ar array
+#define ll long long
+typedef int uci;
+#define int long long
+#define F first
+#define S second
 #define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define irep(i, a, b) for(int i = a; i >= (b); --i)
 #define all(x) begin(x), end(x)
 #define sz(x) (int)(x).size()
-typedef long long ll;
 typedef pair<int, int> pii;
 typedef vector<int> vi;
 
-int main() {
-	cin.tie(0)->sync_with_stdio(0);
-	cin.exceptions(cin.failbit);
+uci main() {
+    cin.tie(0)->sync_with_stdio(0); solve();
 }

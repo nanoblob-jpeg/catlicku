4e41e509a042cbb7d3c4ed07cb00e6b8f06dc1d8
codicon <elijahjhuang@gmail.com>
2024-08-19 00:58:18 -0700

    Trimmed + synergized with 2D

--- content/data-structures/SegmentTree.h
+++ content/data-structures/SegmentTree.h
@@ -4,28 +4,28 @@
  * License: CC0
  * Source: folklore
  * Description: Zero-indexed max-tree. Bounds are inclusive to the left and exclusive to the right.
- * Can be changed by modifying T, f and unit.
+ * Can be changed by modifying \texttt{iv}, \texttt{f};
  * Time: O(\log N)
  * Status: stress-tested
  */
 #pragma once
 
-struct Tree {
-	typedef int T;
-	static constexpr T unit = INT_MIN;
-	T f(T a, T b) { return max(a, b); } // (any associative fn)
-	vector<T> s; int n;
-	Tree(int n = 0, T def = unit) : s(2*n, def), n(n) {}
-	void update(int pos, T val) {
-		for (s[pos += n] = val; pos /= 2;)
-			s[pos] = f(s[pos * 2], s[pos * 2 + 1]);
-	}
-	T query(int b, int e) { // query [b, e)
-		T ra = unit, rb = unit;
-		for (b += n, e += n; b < e; b /= 2, e /= 2) {
-			if (b % 2) ra = f(ra, s[b++]);
-			if (e % 2) rb = f(s[--e], rb);
-		}
-		return f(ra, rb);
-	}
+int iv = INT_MIN;
+int f(int a, int b) { return max(a, b); } // commutative
+
+struct ST {
+  vector<int> s; int n;
+  ST(int n = 0) : s(2*n, iv), n(n) {}
+  void update(int i, int v) {
+    for (s[i += n] = v; i /= 2;)
+      s[i] = f(s[i * 2], s[i * 2 + 1]);
+  }
+  int query(int b, int e) { // query [b, e)
+    int r = iv;
+    for (b += n, e += n; b < e; b /= 2, e /= 2) {
+      if (b % 2) r = f(r, s[b++]);
+      if (e % 2) r = f(s[--e], r);
+    }
+    return r;
+  }
 };

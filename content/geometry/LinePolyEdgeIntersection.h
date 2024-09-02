1ae44b5c1a39f6fad5bdcd32bd64e8e0ce6b618e
Thomas Neill <tommydneill@gmail.com>
2024-09-01 22:31:59 -0500

    Update LinePolyEdgeIntersection.h

--- content/geometry/LinePolyEdgeIntersection.h
+++ content/geometry/LinePolyEdgeIntersection.h
@@ -3,9 +3,9 @@
  * Date: 2024-05-08
  * License: NA
  * Source: thomas
- * Description: Calcs "delta points" for line s-e and edge of some polygon.
-*               To use, sort outputs by first key and maintain running sum of second key. 
-*               != 0 => in polygon.
+ * Description:  For use in computing line-polygon intersection between some line s-e and polygon side p1-p2. 
+ *               To use, apply this function to all polygon edges, sort outputs by first key and maintain running sum of second key. 
+ *               Segments for which the running sum was nonzero are in the intersection.
  */
 vector<pair<P, ll>> linePoly(P s, P e, P p1, P p2) {
     ll s1 = sideOf(s,e,p1), s2 = sideOf(s,e,p2);

c06b1e9c826f489e9d6a08e95496b1eab7676337
codicon <elijahjhuang@gmail.com>
2024-09-10 22:38:21 -0700

    Tailored CRT for most common use case

--- content/number-theory/CRT.h
+++ content/number-theory/CRT.h
@@ -2,10 +2,8 @@
  * Author: Simon Lindholm
  * Date: 2019-05-22
  * License: CC0
- * Description: Chinese Remainder Theorem.
- *
- * \texttt{crt(a, m, b, n)} computes $x$ such that $x\equiv a \pmod m$, $x\equiv b \pmod n$.
- * If $|a| < m$ and $|b| < n$, $x$ will obey $0 \le x < \text{lcm}(m, n)$.
+ * Description: Returns $(x, \text{lcm}(m, n))$ such that $x\equiv a \pmod m$, $x\equiv b \pmod n$.
+ * If $|a| < m$ and $|b| < n$, $x$ will obey $0 \le x < \text{lcm}(m, n)$. Returns $(0, 0)$ if there is no solution.
  * Assumes $mn < 2^{62}$.
  * Time: $\log(n)$
  * Status: Works
@@ -14,10 +12,10 @@
 
 #include "euclid.h"
 
-ll crt(ll a, ll m, ll b, ll n) {
-	if (n > m) swap(a, b), swap(m, n);
-	ll x, y, g = euclid(m, n, x, y);
-	assert((a - b) % g == 0); // else no solution
-	x = (b - a) % n * x % n / g * m + a;
-	return x < 0 ? x + m*n/g : x;
+ar<ll, 2> crt(ll a, ll m, ll b, ll n) {
+    if (n > m) swap(a, b), swap(m, n);
+    ll x, y, g = euclid(m, n, x, y);
+    if ((a - b) % g) return {};
+    x = (b - a) % n * x % n / g * m + a;
+    return {x < 0 ? x + m*n/g : x, m*n/g};
 }
